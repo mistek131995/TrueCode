3517b7f12479b18df19a8a0854a1e52a1d28805f
Юрий Кузнецов <omega131995@outlook.com>
2024-10-13 08:23:45 +0800

    Проверяем строку

--- TrueCode.API/Program.cs
+++ TrueCode.API/Program.cs
@@ -5,9 +5,7 @@ using Queries.Application;
 using TrueCode.API.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
-var connectionString = "Server=mssql;Database=TrueCode;User Id=sa;Password=TrueCode2024@;TrustServerCertificate=True;";
-
-Console.WriteLine(connectionString);
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")!;
 
 builder.Services.AddCommands(connectionString);
 builder.Services.AddQueries(connectionString);
